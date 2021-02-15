a045be158d631da46a46c342b45958f694c7b4bd
Daniel Marques <daniel.marques142@gmail.com>
2021-02-15 20:33:55 -0300

    this keyword

--- Python/uniqueStrings.py
+++ Python/uniqueStrings.py
@@ -1,2 +1,15 @@
 
+#
+# def solve(a,b):
+#     return "".join([i for i in a if not i in b]+[c for c in b if not c in a])
+#
+#
+# a = input()
+# b = input()
+#
+# print(solve(a, b))
 
+
+lista = [11.2, 0, -10, 32]
+
+print(lista % 2 == 0)
