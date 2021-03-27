ac2837c3f3bcee2a5af83ae5fb1ee9f55c44149f
Daniel Marques <daniel.marques142@gmail.com>
2021-03-27 15:48:10 -0300

    regex groups and Or

--- JavaScript/regex/01.js
+++ JavaScript/regex/01.js
@@ -1,7 +1,12 @@
 // - g : Global (Found a pattern in all the file source given)
 // - i : Insesitive (Find only that was passed)
+// - () :  Groups (Find specific groups in strings)
+// - | : Or Operator
 
 const texto  = require('./textBase')
 
-const regExp = /vestido  vermelhor/i
-console.log(regExp.test(texto))
+const regExp = /(namorada | Daniel)/i
+// console.log(regExp.test(texto))
+const found = regExp.exec(texto)
+
+console.log(found[0])
