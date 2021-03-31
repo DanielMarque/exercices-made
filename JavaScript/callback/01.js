b4aefb0180be7cb7fd1f0d518166d2a9f43940db
Daniel Marques <daniel.marques142@gmail.com>
2021-03-31 02:24:54 -0300

    promise

---
+++ JavaScript/callback/01.js
@@ -0,0 +1,22 @@
+console.log('Start')
+
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log('Email Confirmado')
+    callback ({ userEmail: email })
+  }, 3000)
+}
+
+function getVideos(email, callback) {
+  setTimeout(() => {
+    callback (['200', 111, 'Daniel'])
+  }, 2000)
+}
+
+const user = loginUser('daniel@daniel.com', 12345, (user) => {
+  getVideos(user.userEmail, (result) => {
+    console.log(result)
+  })
+})
+
+console.log('Finish')
