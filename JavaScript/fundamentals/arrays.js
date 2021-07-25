765ac15c74819ef9fa5ea80b980dec0eaa7264db
Daniel Marques <daniel.marques142@gmail.com>
2021-07-25 20:20:31 -0300

    destructuring
    removing console
    
    adding console
    
    removing console1

--- JavaScript/fundamentals/arrays.js
+++ JavaScript/fundamentals/arrays.js
@@ -1,6 +1,8 @@
-const lista = [10, 20, 30, 35]
-lista.splice(0, 0, 18, 15, 16)
-console.log(lista)
+const lista = [10, 20, 30, 35];
+lista.splice(0, 0, 18, 15, 16);
+console.log(lista);
 
-const ordenado = lista.sort((a, b) => { return a - b })
-console.log(ordenado)
+const ordenado = lista.sort((a, b) => {
+  return a - b;
+});
+console.log(ordenado);
