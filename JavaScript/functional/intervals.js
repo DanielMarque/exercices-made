73bfea7128361aa749a4e10da173a7d3bc836684
DanielMarquesz <daniel.marques142@gmail.com>
2021-07-27 23:49:38 -0300

    mumbluing

---
+++ JavaScript/functional/intervals.js
@@ -0,0 +1,12 @@
+
+// Interval and Timeout
+
+function write() {
+  console.log('Daniel Marques')
+}
+
+// Function that are called every 2 seconds
+//setInterval(write, 2000)
+
+// Function that is invoced in determinated time
+setTimeout(write, 3000)
