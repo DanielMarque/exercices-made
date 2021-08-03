9364bd372758538d7d4f15d5d48ba5ef0e00b585
DanielMarquesz <daniel.marques142@gmail.com>
2021-08-03 16:06:21 -0300

    filter

--- JavaScript/challengs/destructuring.js
+++ JavaScript/challengs/destructuring.js
@@ -1,34 +1,7 @@
 
-// Destructuring Sample - Array
+const itens = ["banana", "apple", "grape"]
 
-const frutas = ['Banana', 'water melon', 'avocado', 'Apricots']
+const [primeiro, ...newItens] = itens
 
-const [primeira, segunda, ...rest] = frutas
-
-console.log(segunda)
-console.log(rest)
-
-// Spread Sample - Array
-
-const frutasCerameladas = ['apple', ...frutas, 'melon']
-
-frutasCerameladas.pop()
-frutasCerameladas.shift()
-
-console.log(frutasCerameladas)
-
-// Destructuring Sample - Object
-
-const user = {
-  name: 'Daniel',
-  photos: [1, 2, 3, 45],
-  age: 23,
-  analytics: {
-    subs: 300,
-    likes: 12
-  }
-}
-
-const {name, subs} = user.analytics
-
-console.log(subs)
+console.log(primeiro)
+console.log(newItens)
