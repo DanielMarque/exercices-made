30654afa15fa767217d6c6d2d705abee5e05097f
DanielMarquesz <daniel.marques142@gmail.com>
2021-08-03 15:43:22 -0300

    map

---
+++ JavaScript/fundamentals/arrays/03.js
@@ -0,0 +1,9 @@
+
+// Map é utilizado para fazer transformações em um Array, gerando um novo.
+
+const values = [2, 4, 6, 8, 10]
+
+const doubleValues = values.map((valor) => {
+  return ((valor * 2) + 4) / 5
+})
+
